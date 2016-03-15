79923494aa4b828c0fb14bbc0acc95ff90eadaa8
Duncan Regan <duncanregan@gmail.com>
2016-03-15 17:57:20 -0400

    adds add and subtract actions

--- js/actions.js
+++ js/actions.js
@@ -1,2 +1,10 @@
 export const INCREMENT = 'increment';
 export const DECREMENT = 'decrement';
+
+export const add = () => {
+  return {type: INCREMENT};
+};
+
+export const subtract = () => {
+  return {type: DECREMENT};
+};
