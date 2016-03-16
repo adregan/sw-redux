4db62f84c9acbf6f3a9fc24c4b9c58ce97a4f4a8
Duncan Regan <duncanregan@gmail.com>
2016-03-15 22:00:54 -0400

    adds the more listeners and events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,46 @@
-navigator.serviceWorker.register('/static/service.js')
-  .then(reg => {
-    console.log(reg.scope);
-  })
-  .catch(err => console.error(err));
+import { add, subtract } from './actions.js';
+
+const controls = document.getElementById('controls');
+const display = document.getElementById('display');
+
+const receiveMessage = (event) => {
+  display.textContent = event.data;
+};
+
+const sendMessage = (msg) => {
+  let messageChannel = new MessageChannel();
+  messageChannel.port1.onmessage = receiveMessage; 
+  navigator.serviceWorker.controller.postMessage(msg, [messageChannel.port2]);
+};
+
+controls.addEventListener('click', (e) => {
+  e.stopPropagation();
+
+  if (!navigator.serviceWorker.controller) {
+    return alert('So sorry something has gone wrong. Please refresh.');
+  }
+
+  switch (event.target.id) {
+    case 'add':
+      return sendMessage(add());
+    case 'sub':
+      return sendMessage(subtract());
+    default:
+      return;
+  }
+});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service.js')
+    .then((reg) => {
+      console.log('Worker is up.', reg);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  navigator.serviceWorker.addEventListener('message', (event) => {
+    receiveMessage(event);
+  });
+}
+
--- js/service.js
+++ js/service.js
@@ -1,1 +1,51 @@
-console.log('I AM THE SERVICE WORKER');
+import { createStore } from 'redux';
+import count from './reducers.js';
+
+const VERSION = 11;
+const CACHE_NAME = `sw-redux-v${VERSION}`;
+
+const toCache = [
+  '/',
+  '/static/index.js'
+];
+
+let store;
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(toCache))
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request)
+      .then(r => {
+        // Cache hit - return response
+        if (r) {return r;}
+        return fetch(e.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  console.log(`Activated: ${CACHE_NAME}`);
+  store = createStore(count);
+  event.waitUntil(
+    self.clients.claim()
+      .then(() => self.clients.matchAll())
+      .then(clients => {
+        return Promise.all(
+          clients.map(client => client.postMessage(store.getState()))
+        );
+      })
+  ); 
+});
+
+self.addEventListener('message', (e) => {
+  store.dispatch(e.data);
+  e.ports[0].postMessage(store.getState());
+  console.log('message received');
+});
+
