ff712b4e090adbb514d2992c86c4bd4679354afa
Duncan Regan <duncanregan@gmail.com>
2016-03-17 22:24:08 -0400

    cleaning up the install function

--- js/service/install.js
+++ js/service/install.js
@@ -5,27 +5,31 @@ import localforage from 'localforage';
  * @description
  * Constructs the install function for the SW
  * 
- * @param {object} opts
+ * @param {object} opts 
  * @param {string} opts.name      REQUIRED: The name of the app
  * @param {number} opts.version   The version number of the cache
  * @param {array}  opts.items     The urls to cache
  */
-const install = (opts = {}) => {
-  let { name, version, items } = opts;
-  if (!name) {
-    throw TypeError('Missing required parameter: name');
-  }
-  items = (!items) ? ['/'] : items;
-  version = (version !== 0 && !version) ? 0 : version;
-
+const install = ({name, version = 0, items = ['/']}) => {
+  if (!name) throw TypeError('Missing required parameter: name');
   const cacheName = `${name}-v${version}`;
 
   return (event) => {
-    console.log(`Installing SW: ${cacheName}`);
-    // config is a synchronous event
+    console.log(
+      'Installing service worker and database. ' +
+      `Name: ${name}, Version: ${version}`
+    );
+
     localforage.config({name, version, driver: localforage.INDEXEDDB});
     event.waitUntil(
-      caches.open(cacheName).then(cache => cache.addAll(items))
+      new Promise((resolve, reject) => {
+        localforage.getItem('state')
+          .then(s => (!s) ? localforage.setItem('state', {}) : true)
+          .then(() => caches.open(cacheName))
+          .then(cache => cache.addAll(items))
+          .then(() => resolve(true))
+          .catch(err => reject(err));
+      }).catch(err => console.error(err))
     );
   };
 };
