c86abda73c75a019b196bc407e1951d98b88ac95
Duncan Regan <duncanregan@gmail.com>
2016-03-18 15:24:04 -0400

    adds a reset reducer to handle the reset actions

--- js/reducers.js
+++ js/reducers.js
@@ -6,6 +6,8 @@ const count = (state = 0, action) => {
       return state + 1;
     case DECREMENT:
       return state - 1;
+    case 'RESET':
+      return action.state;
     default:
       return state;
   }
