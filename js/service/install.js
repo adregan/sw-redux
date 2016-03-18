f7e3faef388d0cf67f43231752b04f3b5cff1570
Duncan Regan <duncanregan@gmail.com>
2016-03-18 13:42:08 -0400

    removing any references to data

--- js/service/install.js
+++ js/service/install.js
@@ -14,21 +14,16 @@ const install = ({name, version = 1, items = ['/']}) => {
   if (!name) throw TypeError('Missing required parameter: name');
   const cacheName = `${name}-v${version}`;
 
-  console.log('Installing service worker and database. ' +
+  console.log('Installing service worker and configuring database. ' +
     `Name: ${name}, Version: ${version}`);
 
   localforage.config({name, version, driver: localforage.INDEXEDDB});
 
   return (event) => {
     event.waitUntil(
-      new Promise((resolve, reject) => {
-        localforage.getItem('state')
-          .then(s => {return (!s) ? localforage.setItem('state', 0) : true;})
-          .then(() => caches.open(cacheName))
-          .then(cache => cache.addAll(items))
-          .then(() => resolve(true))
-          .catch(err => {console.error(err); reject(err);});
-      })
+      caches.open(cacheName)
+        .then(cache => cache.addAll(items))
+        .catch(err => console.error(err))
     );
   };
 };
