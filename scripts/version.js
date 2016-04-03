9837cee399b1d5c45db4a43db0ea375bb375ab2c
Duncan Regan <duncanregan@gmail.com>
2016-04-03 17:19:12 -0400

    updates script to import config with require

--- scripts/version.js
+++ scripts/version.js
@@ -2,18 +2,13 @@
 
 var fs = require('fs');
 var path = require('path');
+var config = require('../config');
 
 (() => {
   'use strict';
-  
-  const configPath = path.join(__dirname, '..', '/config.json');
 
-  fs.readFile(configPath, (err, data) => {
-    if (err) throw err;
-    let config = JSON.parse(data);
-    config.cache.version += 1;
-  
-    fs.writeFile(configPath, JSON.stringify(config, null, 4), err => {if (err) throw err});
-  });
+  const configPath = path.join(__dirname, '..', '/config.json');
+  config.cache.version += 1;
+  fs.writeFile(configPath, JSON.stringify(config, null, 4), err => {if (err) throw err;});
 
 })();
